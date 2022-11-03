6207a404832ef728681b10d7ec67ee79ca63d824
Sean Yeo <seansqyeo@gmail.com>
2022-11-02 21:17:59 -0400

    add extra routes for api!

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.types import Float, String, Integer
+from sqlalchemy import desc
 from datetime import datetime
 import json
 import jsonpickle
@@ -39,38 +40,128 @@ class WageData(db.Model):
     def __repr__(self) -> str:
         return f'<Task {self.occupation_code}>'
 
+def db_retrieval(wage_data_list):
+    json_list = []
+    useless_key = '_sa_instance_state'
+    for wage_data_class_instance in wage_data_list:
+        wage_data_dict = wage_data_class_instance.__dict__
+
+        # get rid of key just representing db connection information
+        # which isn't useful for displayed data
+        wage_data_dict.pop(useless_key, None)
+
+        # add the data to the list after removing key
+        json_list.append(wage_data_dict)
+
+    # convert the list to a json object and return
+    # (cannot return as a raw list!!!)
+    return json.dumps(json_list)
+
+def general_query_func(data):
+    try:
+        return db_retrieval(data)
+    except Exception as e:
+        print(str(e))
+        return 'An error has occured'
+
+def read_data(try_except_func, data):
+    return try_except_func(data)
+
 @app.route('/data/<county_code>', methods=['GET'])
-def data(county_code):
+def all_county_data(county_code):
     if request.method == 'GET':
+        # queries a list of WageData objects
+        # where each WageData object represents a row from
+        # a specified county wage data table
+        # filter it by county_code that's passed in the url
+        return read_data(
+            general_query_func,
+            WageData.query.filter(WageData.county_code == county_code)
+        )
 
-        try:
-            # generate the class for the table
-            # db_class = type(f'county_{county_code}', (WageDataBaseClass, db.Model), {'__tablename__': f'county_{county_code}'})
+@app.route('/data/sortDescending/medianHourlyWage', methods=['GET'])
+def descending_median_hourly_wage():
+    if request.method == 'GET':
+        return read_data(
+            general_query_func,
+            WageData.query.filter(WageData.median_hourly_wage_usd != None).
+            order_by(desc(WageData.median_hourly_wage_usd)).all()
+        )
 
-            # queries a list of WageData objects
-            # where each WageData object represents a row from
-            # a specified county wage data table
-            # filter it by county_code that's passed in the url
-            wage_data_list = WageData.query.filter(WageData.county_code == county_code)
+@app.route('/data/sortDescending/meanHourlyWage', methods=['GET'])
+def descending_mean_hourly_wage():
+    if request.method == 'GET':
+        return read_data(
+            general_query_func,
+            WageData.query.filter(WageData.mean_hourly_wage_usd != None).
+            order_by(desc(WageData.mean_hourly_wage_usd)).all()
+        )
 
-            json_list = []
-            useless_key = '_sa_instance_state'
-            for wage_data_class_instance in wage_data_list:
-                wage_data_dict = wage_data_class_instance.__dict__
+@app.route('/data/sortDescending/annualWage', methods=['GET'])
+def descending_annaul_wage():
+    if request.method == 'GET':
+        return read_data(
+            general_query_func,
+            WageData.query.filter(WageData.annual_mean_wage_usd != None).
+            order_by(desc(WageData.annual_mean_wage_usd)).all()
+        )
 
-                # get rid of key just representing db connection information
-                # which isn't useful for displayed data
-                wage_data_dict.pop(useless_key, None)
+@app.route('/data/sortDescending/employmentPerThousand', methods=['GET'])
+def descending_employment_per_1000():
+    if request.method == 'GET':
+        return read_data(
+            general_query_func,
+            WageData.query.filter(WageData.employment_per_1000_jobs != None).
+            order_by(desc(WageData.employment_per_1000_jobs)).all()
+        )
 
-                # add the data to the list after removing key
-                json_list.append(wage_data_dict)
+@app.route('/data/ascendingDescending/medianHourlyWage', methods=['GET'])
+def ascending_median_hourly_wage():
+    if request.method == 'GET':
+        return read_data(
+            general_query_func,
+            WageData.query.filter(WageData.median_hourly_wage_usd != None).
+            order_by(WageData.median_hourly_wage_usd).all()
+        )
 
-            # convert the list to a json object and return
-            # (cannot return as a raw list!!!)
-            return json.dumps(json_list)
-        except Exception as e:
-            print(str(e))
-            return "The given countycode does not exist!"
+@app.route('/data/sortAscending/meanHourlyWage', methods=['GET'])
+def ascending_mean_hourly_wage():
+    if request.method == 'GET':
+        return read_data(
+            general_query_func,
+            WageData.query.filter(WageData.mean_hourly_wage_usd != None).
+            order_by(WageData.mean_hourly_wage_usd).all()
+        )
+@app.route('/data/sortAscending/annualWage', methods=['GET'])
+def ascending_annaul_wage():
+    if request.method == 'GET':
+        return read_data(
+            general_query_func,
+            WageData.query.filter(WageData.annual_mean_wage_usd != None).
+            order_by(WageData.annual_mean_wage_usd).all()
+        )
+
+@app.route('/data/sortAscending/employmentPerThousand', methods=['GET'])
+def ascending_employment_per_1000():
+    if request.method == 'GET':
+        return read_data(
+            general_query_func,
+            WageData.query.filter(WageData.employment_per_1000_jobs != None).
+            order_by(WageData.employment_per_1000_jobs).all()
+        )
+
+
+# have a route where user can pass in field to order by and choose descending or ascending!!!
+# for example, would be really good to be able to sort by employment per 1000 descending
+# followed by sort by a wage column descending
+# that way can see which occupation has jobs available with decent pay!!!
+
+# should be able to eventually search by occupation title in UI
+# maybe map to occupation_code
+
+# eventually filter by state and occupation
+
+# need a table for state, county code (like a meta table?)
 
 
 if __name__ == "__main__":
